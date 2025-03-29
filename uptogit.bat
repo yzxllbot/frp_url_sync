70ea9c66c853436223bc78b3efe5d374abfde6fa
YiZhiXiaoLiuLang <3035969948@qq,com>
2025-03-29 11:54:48 +0800

    bot-commit-v1

---
+++ uptogit.bat
@@ -0,0 +1,7 @@
+C:
+cd C:\Users\Administrator\Desktop\runpy\output
+git remote set-url origin git@github.com:yzxllbot/frp_url_sync.git
+git remote set-url origin git@github-repo-frp:yzxllbot/frp_url_sync.git
+git add .
+git commit -m "bot-commit-v1"
+git push -f origin master
